ae615d2c5e1b94d00d76d36477313147d26d3ee5
unknown <tkfydtls464@gmail.com>
2023-07-23 21:45:33 +0900

    building sign up page

--- src/routes/Signup.js
+++ src/routes/Signup.js
@@ -1,10 +1,10 @@
-import React, {useState} from 'react';
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import React, {useState, useEffect} from 'react';
+import { getAuth, createUserWithEmailAndPassword, fetchSignInMethodsForEmail } from "firebase/auth";
 import {Link} from "react-router-dom";
 
 const Signup = () => {
     const next = (name, email, year, month, day) => {
-        console.log(name, email, year, month, day)
+        setfirstpage(false);
         return(
             <div>
                 <div>{name}</div>
@@ -12,21 +12,11 @@ const Signup = () => {
                 <div>{year}</div>
                 <div>{month}</div>
                 <div>{day}</div>
-                <input type="submit" value="Sign up" />
+                <input onClick={Signup} type="submit" value="가입" />
             </div>
     )
     };
-    /* const year = () => {
-        const empty = []
-        for(let i=1960; i<2024; i++){
-            const cash = {}
-            cash['value'] = i;
-            empty.add(cash)
-        }
-        console.log(empty)
-        return empty
-    }
-    */
+
     const years = [
         { value: "apple", name: "year" },
         { value: "banana", name: "year" },
@@ -49,7 +39,9 @@ const Signup = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [comp, setComp] = useState();
-
+    const [isregisterd, setisregisterd] = useState(0);
+    const [firstpage, setfirstpage] = useState(true);
+    const [error, seterror] = useState(0);
     const dateChange = (event) => {
         console.log(event)
         const {
@@ -68,15 +60,32 @@ const Signup = () => {
         const {
             target: { name, value },
         } = event;
-        if (name === "email") {
-            setEmail(value);
-        } else if (name === "password") {
+        if (name === "password") {
             setPassword(value);
         }
         else{
             setname(value);
         }
     };
+
+    const emailchange = async (event)  => {
+        const {
+            target: { name, value },
+        } = event;
+        setEmail(value);
+        const auth = getAuth();
+        await fetchSignInMethodsForEmail(auth, value)
+            .then((result) => {
+                console.log(result, result.length)
+                setisregisterd(result.length);
+                seterror(0);
+            }).catch((error) => {
+            const errorMessage = error.message;
+            console.log(errorMessage)
+            seterror(1);
+            // ..
+        });
+    }
     const SelectBox = (props) => {
         return (
             <select>
@@ -92,57 +101,70 @@ const Signup = () => {
             </select>
         );
     };
-    const onSubmit = async (event) => {
+    const Signup = async (event) => {
         event.preventDefault();
         const auth = getAuth();
-        console.log(auth)
         createUserWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
                 // Signed in
                 const user = userCredential.user;
-                console.log(user)
+                console.log(user);
                 // ...
             })
             .catch((error) => {
-                const errorCode = error.code;
                 const errorMessage = error.message;
-                console.log(errorMessage)
+                console.log(errorMessage);
+                seterror(2);
                 // ..
             });
     };
+
     return (
         <div>
-            <form onSubmit={onSubmit}>
-                <input
-                    name="name"
-                    type="text"
-                    placeholder="name"
-                    required
-                    value={name}
-                    onChange={onChange}
-                />
-                <input
-                    name="email"
-                    type="text"
-                    placeholder="Email"
-                    required
-                    value={email}
-                    onChange={onChange}
-                />
-                <input
-                    name="password"
-                    type="password"
-                    placeholder="Password"
-                    required
-                    value={password}
-                    onChange={onChange}
-                />
-                <SelectBox options={years} onChange={dateChange} defaultValue="1998"></SelectBox>
-                <SelectBox options={months} onChange={dateChange} defaultValue="04"></SelectBox>
-                <SelectBox options={days} onChange={dateChange} defaultValue="16"></SelectBox>
-                <button onClick={() => setComp(next(name, email, year, month, day))}>next</button>
+            {
+                firstpage ?
+                    <>
+                        <form >
+                            <input
+                                name="name"
+                                type="text"
+                                placeholder="이름"
+                                required
+                                value={name}
+                                onChange={onChange}
+                            />
+                            <input
+                                name="email"
+                                type="text"
+                                placeholder="이메일"
+                                required
+                                value={email}
+                                onChange={emailchange}
+                            />
+                            <div>
+                                {error === 1 && <p>올바른 이메일을 입력해 주세요.</p>}
+                                {(error === 2 || isregisterd > 0) && <p>이미 등록된 이메일입니다.</p>}
+                            </div>
+                            <input
+                                name="password"
+                                type="password"
+                                placeholder="비밀번호"
+                                required
+                                value={password}
+                                onChange={onChange}
+                            />
+                            <SelectBox options={years} onChange={dateChange} defaultValue="1998"></SelectBox>
+                            <SelectBox options={months} onChange={dateChange} defaultValue="04"></SelectBox>
+                            <SelectBox options={days} onChange={dateChange} defaultValue="16"></SelectBox>
+                        </form>
+                        <button disabled={error === 0 && email.length && password.length && name.length !== 0 ? false : true} onClick={() => setComp(next(name, email, year, month, day))}>다음</button>
+                    </>
+
+                :
                 <div children={comp} />
-            </form>
+            }
+
+
         </div>
     );
 };
