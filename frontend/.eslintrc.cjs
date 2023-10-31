04b331c9997a96866a1c75c490d3a548626a88ef
Early Riser <tkfydtls464@gmail.com>
2023-10-31 11:19:46 +0900

    env: ESlint와prettier 설정 완료

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,20 +1,16 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  env: {
+    browser: true,
+    es6: true,
   },
-}
+  extends: [
+    'eslint:airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    // 들여쓰기를 탭으로 2번만 하도록 설정
+    'indent': ['error', 'tab'],
+  },
+};
