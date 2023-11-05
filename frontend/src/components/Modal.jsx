0f5408e1399a1824254adf5cab329862c7ba55bd
Early Riser <tkfydtls464@gmail.com>
2023-11-05 11:01:00 +0900

    feat: 에러 메시지 모달창에 표시하게 추가, 3초후에 꺼짐

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../style/Modal.css';
+import { errorState } from '../recoil/recoil.jsx';
+import { useRecoilState, useRecoilValue } from 'recoil';
 
 const Modal = ({ children }) => {
+  const [error, setError] = useRecoilState(errorState);
+
+  useEffect(() => {
+    if (error) {
+      setTimeout(() => {
+        setError('');
+      }, 3000); // reset the error after 3 seconds
+    }
+  }, [error, setError]);
   return (
     <div className="modalDiv">
       <div className="modal">{children}</div>
+      {error && <div className={'errorMessageDiv'}>{error}</div>}
     </div>
   );
 };
