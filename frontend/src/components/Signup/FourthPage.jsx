673b570129f46d76df7bd874c0864da7b768c28d
Early Riser <tkfydtls464@gmail.com>
2023-11-09 10:11:07 +0900

    fix: 회원가입 3번째 페이지 css 버튼 튀어나오는거 수정

--- frontend/src/components/Signup/FourthPage.jsx
+++ frontend/src/components/Signup/FourthPage.jsx
@@ -1,9 +1,4 @@
 import React from 'react';
-import {
-  createUserWithEmailAndPassword,
-  getAuth,
-  updateProfile,
-} from 'firebase/auth';
 import { dbService } from '../../fbase';
 import { doc, setDoc, collection } from 'firebase/firestore';
 import '../../style/SignupFourthPage.css';
@@ -13,36 +8,8 @@ const FourthPage = ({ onNext, onPrev, user_data, page, setPage }) => {
     onPrev();
   };
 
-  const Signup = async (event) => {
-    event.preventDefault();
-    const auth = getAuth();
-    createUserWithEmailAndPassword(auth, user_data.email, user_data.password)
-      .then((userCredential) => {
-        // Signed in
-        const user = userCredential.user;
-        Profile_toDB(user, user_data);
-        updateProfile(user, {
-          displayName: user_data.name,
-          photoURL:
-            'https://firebasestorage.googleapis.com/v0/b/loginform-6747a.appspot.com/o/pfp%2Fbasic.png?alt=media&token=d2b2f037-ee93-4fad-a09d-733332ec28fc',
-        })
-          .then(() => {
-            // Profile updated!
-          })
-          .catch((error) => {
-            // An error occurred
-            console.log(error.message);
-          });
-        onNext();
-        setModals(false);
-        console.log('회원가입 완료');
-        // ...
-      })
-      .catch((error) => {
-        const errorMessage = error.message;
-        console.log(errorMessage);
-        // ..
-      });
+  const Signup = async () => {
+    onNext();
   };
 
   const Profile_toDB = async (userObj, user_data) => {
