7a5062999e2dbe8ab4cd443523bafa0cfacfc630
Early Riser <tkfydtls464@gmail.com>
2023-11-12 14:55:38 +0900

    feat: 프로필 업데이트 react query hook 업데이트

--- src/hooks/useUpdateProfile.jsx
+++ src/hooks/useUpdateProfile.jsx
@@ -1,11 +1,20 @@
 import { useMutation } from 'react-query';
 import axios from 'axios';
 
-const useUpdateProfile = () =>
-  useMutation((userData) => {
-    axios.post(
-      `${import.meta.env.VITE_REACT_APP_BACKEND_URL}/updateProfile`, { uid: userData.uid, name: userData.name, photoURL: userData.photoURL }
-    );
-  });
+const useUpdateProfile = () =>{
+    return useMutation(async ({userData}) => {
+        const response = await axios.post(
+            `${import.meta.env.VITE_REACT_APP_BACKEND_URL}/updateProfile`, { uid: userData.uid, name: userData.name, photoURL: userData.photoURL }
+        );
+        if (response.status !== 200) {
+            throw new Error(
+                response.data.message || '프로필 업데이트 중 오류가 발생했습니다.',
+            );
+        }
+        return response.data;
+    });
+
+}
+
 
 export default useUpdateProfile;
