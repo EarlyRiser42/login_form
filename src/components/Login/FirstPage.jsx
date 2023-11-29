ca688b343d03fcfffffa386295701b5eebd1581a
Early Riser <tkfydtls464@gmail.com>
2023-11-29 09:48:38 +0900

    refacotr: 로그인 첫 페이지 styled component화

--- src/components/Login/FirstPage.jsx
+++ src/components/Login/FirstPage.jsx
@@ -4,7 +4,7 @@ import { authService, firebaseInstance } from '../../fbase';
 import { useRecoilState } from 'recoil';
 import { errorState, loginState, ModalOpenState } from '../../util/recoil.jsx';
 import axios from 'axios';
-import '../../style/Login/LoginFirstPage.css';
+import styled from 'styled-components';
 
 const FirstPage = ({ onNext }) => {
   const [email, setEmail] = useState('');
@@ -29,7 +29,10 @@ const FirstPage = ({ onNext }) => {
     const requestData = isEmail ? { email: value } : { id: value };
 
     try {
-      const response = await axios.post(`${import.meta.env.VITE_REACT_APP_BACKEND_URL}/checkEmailOrId`, requestData);
+      const response = await axios.post(
+        `${import.meta.env.VITE_REACT_APP_BACKEND_URL}/checkEmailOrId`,
+        requestData,
+      );
 
       if (response.status === 200 && response.data && response.data.exists) {
         onNext({ email: value });
@@ -71,30 +74,21 @@ const FirstPage = ({ onNext }) => {
   };
 
   return (
-    <div className={'LoginModal'}>
-      <div className={'LoginLogoDiv'}>
-        <div className={'LoginCloseButtonDiv'}>
+    <LoginModal>
+      <LoginLogoDiv>
+        <LoginCloseButtonDiv>
           <Link to={'/'}>
-            <button
-              className={'LoginCloseButton'}
-              onClick={() => {
-                setIsModalOpen(false);
-              }}
-            >
-              <img
-                className={'LoginCloseImg'}
-                src="/close.svg"
-                alt="close button"
-              />
-            </button>
+            <LoginCloseButton onClick={() => setIsModalOpen(false)}>
+              <LoginCloseImg src="/close.svg" alt="close button" />
+            </LoginCloseButton>
           </Link>
-        </div>
-        <img className={'LoginXLogo'} src="/X_logo.svg" alt="X logo" />
-      </div>
-      <div className={'Loginh1Div'}>
-        <span className={'Loginh1'}>X 가입하기</span>
-      </div>
-      <div className={'LoginButtonDiv'}>
+        </LoginCloseButtonDiv>
+        <LoginXLogo src="/X_logo.svg" alt="X logo" />
+      </LoginLogoDiv>
+      <Loginh1Div>
+        <Loginh1>X 가입하기</Loginh1>
+      </Loginh1Div>
+      <LoginButtonDiv>
         <AuthButton
           name="google"
           onClick={onSocialClick}
@@ -108,13 +102,7 @@ const FirstPage = ({ onNext }) => {
           text="Github으로 로그인하기"
         />
         <div>
-          <div className={'line'}>
-            <span style={{ fontWeight: 'normal', color: 'black' }}>또는</span>
-          </div>
-        </div>
-        <div>
-          <input
-            className={'LoginInput'}
+          <LoginInput
             name="email"
             type="email"
             placeholder="휴대폰 번호, 이메일 주소 또는 사용자 아이디"
@@ -124,28 +112,194 @@ const FirstPage = ({ onNext }) => {
           />
         </div>
         <div>
-          <button className={'LoginNextButton'} onClick={() => onClick(email)}>
-            다음
-          </button>
+          <LoginNextButton onClick={() => onClick(email)}>다음</LoginNextButton>
         </div>
         <div>
-          <button
-            className={'LoginPasswordResetButton'}
+          <LoginPasswordResetButton
             onClick={() => setRecoilError('구현중인 기능입니다.')}
           >
             비밀번호를 잊으셨나요?
-          </button>
+          </LoginPasswordResetButton>
         </div>
-      </div>
-
-      <div className={'Loginh4Div'}>
-        <span className={'Loginh4'}>계정이 없으신가요?</span>
-        <Link className={'LoginSignupLink'} to={'/signup'}>
-          <span className={'LoginSignupLink'}>가입하기</span>
+      </LoginButtonDiv>
+      <Loginh4Div>
+        <span>계정이 없으신가요?</span>
+        <Link to={'/signup'}>
+          <LoginSignupLink>가입하기</LoginSignupLink>
         </Link>
-      </div>
-    </div>
+      </Loginh4Div>
+    </LoginModal>
   );
 };
 
+const LoginModal = styled.div`
+  width: 590px;
+  height: 650px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+
+  @media (max-width: 700px) {
+    width: 100vw;
+    height: 100vh;
+  }
+
+  @media (max-width: 480px) {
+    width: 100vw;
+    height: 90vh;
+  }
+`;
+
+const LoginLogoDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  justify-content: flex-start;
+  align-items: center;
+  margin-top: 2%;
+`;
+
+const LoginCloseButtonDiv = styled.div`
+  margin-left: 4%;
+  margin-right: 37%;
+  height: 100%;
+
+  @media (max-width: 700px), (max-width: 480px) {
+    margin-left: 4%;
+    margin-right: 37%;
+    margin-top: 0%;
+  }
+`;
+
+const LoginCloseButton = styled.button`
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  width: 90%;
+  height: 100%;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &:hover {
+    background-color: #e7e7e8;
+  }
+`;
+
+const LoginCloseImg = styled.img`
+  width: 20px;
+  height: 20px;
+`;
+
+const LoginXLogo = styled.img`
+  width: 30px;
+  height: 25px;
+`;
+
+const Loginh1Div = styled.div`
+  width: 100%;
+  max-width: 300px;
+  margin-bottom: 15%;
+`;
+
+const Loginh1 = styled.span`
+  font-weight: bold;
+  font-size: 2rem;
+`;
+
+const LoginButtonDiv = styled.div`
+  width: 100%;
+  height: auto;
+  min-height: 350px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  margin-top: -20%;
+
+  @media (max-width: 700px) {
+    margin-top: -30%;
+  }
+
+  @media (max-width: 480px) {
+    margin-top: -30%;
+  }
+`;
+
+const LoginInput = styled.input`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  width: 255px;
+  height: 20px;
+  padding: 10px 20px;
+  border: 1px solid #ccc;
+  background-color: white;
+  font-size: 16px;
+  color: black;
+  cursor: pointer;
+  text-decoration: none;
+`;
+
+const LoginNextButton = styled.button`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  width: 300px;
+  height: 40px;
+  padding: 10px 20px;
+  border-radius: 25px;
+  border: 1px solid #ccc;
+  background-color: black;
+  font-size: 16px;
+  font-weight: 550;
+  color: white;
+  cursor: pointer;
+  text-decoration: none;
+`;
+
+const LoginPasswordResetButton = styled.button`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  width: 300px;
+  height: 40px;
+  padding: 10px 20px;
+  border-radius: 25px;
+  border: 1px solid #ccc;
+  background-color: white;
+  font-size: 16px;
+  font-weight: 550;
+  color: black;
+  cursor: pointer;
+  text-decoration: none;
+
+  &:hover {
+    background-color: #e7e7e8;
+  }
+`;
+
+const Loginh4Div = styled.div`
+  width: 100%;
+  max-width: 300px;
+  margin-bottom: 10%;
+  @media (max-width: 700px) {
+    max-width: 290px;
+    margin-top: -10%;
+    margin-bottom: 10%;
+  }
+
+  @media (max-width: 480px) {
+    margin-top: -10%;
+    margin-bottom: 10%;
+  }
+`;
+const LoginSignupLink = styled.span`
+  margin-left: 2%;
+  text-decoration: none; /* 밑줄 제거 */
+  color: #4a99e9; /* 글씨색을 파란색으로 */
+  cursor: pointer;
+`;
 export default FirstPage;
